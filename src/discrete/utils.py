3876b19fc9f753add476e38cedabbf210e2ca127
naltukhov <naltukhov@ourgapps.com>
2024-03-20 18:10:29 +0400

    add sample size estimation for discrete var

---
+++ src/discrete/utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy import stats
+
+
+def calculate_sample_size(p, alpha, power, mde) -> int:
+    """
+    Calculate sample size need for significant test using p, alpha, power, mde
+    :param p: probability of positive sample
+    :param alpha: level of significance of A/B test
+    :param power: probability of observing a statistically significant result at level alpha
+    if a true effect of a certain magnitude is present
+    :param mde: minimal detectable effect, the difference in results to detect
+    :return: sample size needed for each group
+    """
+    z = stats.norm.ppf(q=1 - alpha / 2) + stats.norm.ppf(q=power)
+    size = 2 * p * (1 - p) * (z / mde) ** 2
+    return round(size) + 1
+
+
+def estimate_ci_binomial(p, sample_size, alpha):
+    """
+    Confidence interval for Binomial variable
+    :param p: probability of positive
+    :param sample_size: number of samples
+    :param alpha: alpha level
+    :return: low confidence interval value, high confidence interval value
+    """
+    t = stats.norm.ppf(1 - alpha / 2, loc=0, scale=1)
+    std_n = np.sqrt(p * (1 - p) / sample_size)
+    return p - t * std_n, p + t * std_n
---
+++ tests/test_discrete_utils.py
@@ -0,0 +1,16 @@
+import unittest
+from src.discrete.utils import calculate_sample_size
+from src.discrete.utils import estimate_ci_binomial
+
+
+class TestSampleSizeEstimation(unittest.TestCase):
+    def test_calculate_sample_size_by_mde(self):
+        sample_size = calculate_sample_size(0.07, 0.05, 0.8, 0.03)
+        self.assertTrue(isinstance(sample_size, int), "Estimated sample size has wrong type")
+        self.assertTrue(sample_size > 0, f"Estimated sample size has negative value: {sample_size}")
+
+    def test_estimate_ci_binomial(self):
+        ci = estimate_ci_binomial(0.07, 1136, 0.5)
+        self.assertTrue(len(ci) == 2, f"Got wrong confidence interval: {ci}")
+        self.assertTrue(ci[0] > 0, f"Estimated ci has negative value: {ci[0]}")
+        self.assertTrue(ci[1] > 0, f"Estimated ci has negative value: {ci[1]}")
