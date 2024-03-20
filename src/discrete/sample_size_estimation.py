5d98a857e88a3458db3ce9838534b97c9ba4d829
naltukhov <naltukhov@ourgapps.com>
2024-03-20 15:01:59 +0400

    naming

--- src/discrete/sample_size_estimation.py
+++ src/discrete/sample_size_estimation.py
@@ -2,14 +2,14 @@ from scipy import stats
 import numpy as np
 
 
-def estimate_ci_binomial(p, n, alpha=0.05):
+def estimate_ci_binomial(p, sample_size, alpha=0.05):
     """
     Confidence interval for Binomial variable
     :param p: probability of positive
-    :param n: number of samples
+    :param sample_size: number of samples
     :param alpha: alpha level
     :return: lower confidence interval value, higher confidence interval value
     """
     t = stats.norm.ppf(1 - alpha / 2, loc=0, scale=1)
-    std_n = np.sqrt(p * (1 - p) / n)
+    std_n = np.sqrt(p * (1 - p) / sample_size)
     return p - t * std_n, p + t * std_n
