9008142ab8ce799c32be4c4cd92fb887ed4b7727
naltukhov <naltukhov@ourgapps.com>
2024-04-19 18:27:08 +0400

    add mde estimation by sample size for discrete variables

--- abtoolkit/discrete/utils.py
+++ abtoolkit/discrete/utils.py
@@ -40,6 +40,42 @@ def estimate_sample_size_by_mde(
     return round(size) + 1
 
 
+def estimate_mde_by_sample_size(
+    p: float,
+    alpha: float,
+    power: float,
+    sample_size: int,
+    alternative: Literal["less", "greater", "two-sided"],
+) -> float:
+    """
+    Estimate MDE by need for significant test using p, alpha, power, sample_size
+    :param p: probability of positive sample
+    :param alpha: level of significance of A/B test
+    :param power: probability of observing a statistically significant result at level alpha
+    if a true effect of a certain magnitude is present
+    :param sample_size: number of users in each group (test and control)
+    :param alternative: alternative hypothesis ("less", "greater" or "two-sided").
+    * 'two-sided' : means are equal;
+    * 'less': the mean of the control sample is less than the mean of the test sample;
+    * 'greater': the mean of the control sample is greater than the mean of the test sample;
+    :return: minimum detectable effect
+    """
+
+    # Todo: When alternative = "less" or "greater" we get undersampling (power less then 0.8).
+    if alternative == "two-sided":
+        pass
+
+    alpha = alpha / 2
+
+    z = stats.norm.ppf(q=1 - alpha) + stats.norm.ppf(q=power)
+    # size = 2 * p * (1 - p) * (z / mde) ** 2
+
+    mde = z / np.sqrt(sample_size / (2 * p * (1 - p)))
+
+    return mde
+
+
+
 def estimate_ci_binomial(p, sample_size, alpha):
     """
     Confidence interval for Binomial variable
--- tests/test_discrete_utils.py
+++ tests/test_discrete_utils.py
@@ -1,5 +1,6 @@
 import unittest
 from abtoolkit.discrete.utils import estimate_sample_size_by_mde
+from abtoolkit.discrete.utils import estimate_mde_by_sample_size
 from abtoolkit.discrete.utils import estimate_ci_binomial
 
 
@@ -8,6 +9,11 @@ class TestSampleSizeEstimation(unittest.TestCase):
         sample_size = estimate_sample_size_by_mde(0.07, 0.05, 0.8, 0.03, alternative="two-sided")
         self.assertTrue(isinstance(sample_size, int), "Estimated sample size has wrong type")
         self.assertTrue(sample_size > 0, f"Estimated sample size has negative value: {sample_size}")
+
+    def test_calculate_mde_by_sample_size(self):
+        mde = estimate_mde_by_sample_size(0.07, 0.05, 0.8, 1136, alternative="two-sided")
+        self.assertTrue(isinstance(mde, float), "Estimated MDE has wrong type")
+        self.assertTrue(mde > 0, f"Estimated MDE is negative: {mde}")
 
     def test_estimate_ci_binomial(self):
         ci = estimate_ci_binomial(0.07, 1136, 0.5)
