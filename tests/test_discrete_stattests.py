3b01c31aa29800f0b90ab3f583e34977905ca568
naltukhov <naltukhov@ourgapps.com>
2024-10-10 16:58:59 +0400

    unit tests

--- tests/test_discrete_stattests.py
+++ tests/test_discrete_stattests.py
@@ -2,11 +2,12 @@ import unittest
 
 import numpy as np
 from abtoolkit.discrete.stattests import conversion_ztest
+from abtoolkit.discrete.stattests import bayesian_test
 from abtoolkit.utils import generate_data
 
 
 class TestStatTests(unittest.TestCase):
-    def test_ttest(self):
+    def test_ztest(self):
         test_sr = generate_data(100, distribution_type="disc")
         control_sr = generate_data(100, distribution_type="disc")
         p_value = conversion_ztest(
@@ -16,3 +17,14 @@ class TestStatTests(unittest.TestCase):
             len(test_sr),
             "less")
         self.assertTrue(0 <= p_value <= 1, f"Wrong value for p-value: {p_value}")
+
+    def test_bayesian_test(self):
+        test_sr = generate_data(100, distribution_type="disc")
+        control_sr = generate_data(100, distribution_type="disc")
+        p_value = bayesian_test(
+            control_sr.sum(),
+            len(control_sr),
+            test_sr.sum(),
+            len(test_sr))
+        self.assertTrue(0 <= p_value <= 1, f"Wrong value for p-value: {p_value}")
+
